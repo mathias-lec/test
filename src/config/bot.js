8812d879e4365a633812e5436be218704d862e6e
mathias-lec <141534765+mathias-lec@users.noreply.github.com>
2026-05-25 14:19:40 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Bot de Emma :nekowhoua: caca ",
+        name: "Bot de Emma :nekowhoua: <:nekowhoua:1508442316556206142> ",
         // Activity type number (0 = Playing).
         type: 4, 
       },
